a018bcbf00061d455a9f9df874e292f6108dc677
mlidbo <mlidbo@manpower.se>
2011-05-30 18:43:38 +0200

    Apparently works fine!

--- Application.cs
+++ Application.cs
@@ -2,6 +2,8 @@
 
 using System;
 using System.Collections.Generic;
+using Castle.MicroKernel.Registration;
+using Castle.Windsor;
 
 #endregion
 
@@ -11,7 +13,44 @@ namespace Scratch
     {
         public static void Main()
         {
-            Console.WriteLine("Hello World");
+            var container = new WindsorContainer();
+            container.Register(AllTypes.FromThisAssembly().BasedOn(typeof(IHandle<>)).WithService.Base());
+
+            var handlers = container.ResolveAll<IHandle<IB>>();
+            foreach(var handler in handlers)
+            {
+                handler.Handle(new B() { });
+            }
         }
     }
+
+    public interface IHandle<in T>
+    {
+        void Handle(T message);
+    }
+
+    public interface IA{}
+
+    public interface IB : IA{}
+
+    public class A : IA{}
+    public class B : IB {}
+
+
+    public class AHandler : IHandle<IA>
+    {
+        public void Handle(IA message)
+        {
+            Console.WriteLine("AHandler");
+        }
+    }
+
+    public class IBHandler : IHandle<IB>
+    {
+        public void Handle(IB message)
+        {
+            Console.WriteLine("IBHandler");
+        }
+    }
+
 }
